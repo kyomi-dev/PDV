9694d52aa4126ba8db1ffb8867de5013c7604724
kyomi <104471832+kyomi-dev@users.noreply.github.com>
2024-01-21 14:02:18 -0300

    docs: Define a estrutura inicial das rotas

---
+++ src/rotas/rotas.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const { cadastrarUsuario, logarUsuario, listarCategorias, detalharPerfil, editarPerfil } = require("../controladores/controladores");
+
+
+// ---------- Middlewares ----------
+const validarUsuario = require("../middlewares/validarUsuario");
+const schemaValidacao = require("../validacoes/schemaValidacao");
+const validarToken = require("../middlewares/validarToken");
+const validarLogin = require("../middlewares/validarLogin");
+
+const rotas = express();
+
+
+// listar as categorias do banco de dados
+rotas.get("/categorias", listarCategorias);
+
+// fazer o cadastro do usuario
+rotas.post("/usuario", validarUsuario(schemaValidacao), cadastrarUsuario);
+
+// fazer o login do usuario
+rotas.post("/login", validarLogin, logarUsuario);
+
+// detalhar perfil do usuario
+rotas.get("/usuario/:id", validarToken, detalharPerfil);
+
+// editar perfil do usuario
+rotas.put("/usuario/:id", validarToken, editarPerfil);
+
+
+module.exports = rotas; 
