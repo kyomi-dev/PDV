4e629f934f84266f65c6e1479e1f43304a9d305e
Larissa Boina <boina.larissa1@gmail.com>
2024-01-21 21:30:21 -0300

    feat: Implementa validacao no corpo da requisição

--- src/validacoes/schemaValidacao.js
+++ src/validacoes/schemaValidacao.js
@@ -1,4 +1,20 @@
-const validarUsuario = joiSchema => async (req, res, next) => { }
+const joi = require("joi")
 
+const schemaValidacao = joi.object({
+    nome: joi.string().required().messages({
+        "any.required": "O campo nome é obrigatório",
+        "string.empty": "O campo nome é obrigatório"
+    }),
+    email: joi.string().email().required().messages({
+        "string.email": "O campo email precisa ter um formato válido",
+        "any.required": "O campo email é obrigatório",
+        "string.empty": "O campo email é obrigatório"
+    }),
+    senha: joi.string().min(5).required().messages({
+        "any.required": "O campo senha é obrigatório",
+        "string.min": "A senha precisa ter no mínimo 5 caracteres",
+        "string.empty": "O campo senha é obrigatório"
+    }),
+});
 
-module.exports = validarUsuario; 
+module.exports = schemaValidacao; 
