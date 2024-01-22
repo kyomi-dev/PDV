51a996bafe03e5b8594e2d9c743ccff19bee625f
Larissa Boina <boina.larissa1@gmail.com>
2024-01-21 21:32:33 -0300

    feat: Implementa middleware para validação de usuário

--- src/middlewares/validarUsuario.js
+++ src/middlewares/validarUsuario.js
@@ -1,4 +1,11 @@
-const validarUsuario = joiSchema => async (req, res, next) => { }
+const validarUsuario = joiSchema => async (req, res, next) => {
+    try {
+        await joiSchema.validateAsync(req.body);
+        next();
+    } catch (error) {
+        return res.status(500).json(error.message)
+    }
+};
 
 
 module.exports = validarUsuario; 
