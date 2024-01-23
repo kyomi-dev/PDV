4366f171d44df198ec3b0b64030b7e28aa03b34b
kyomi <104471832+kyomi-dev@users.noreply.github.com>
2024-01-23 09:35:15 -0300

    feat: Cria arquivo que gera as tabelas no banco de dados

---
+++ tabelas.sql
@@ -0,0 +1,29 @@
+/ / 1a Sprint 
+
+CREATE TABLE usuarios (
+    id SERIAL NOT NULL PRIMARY KEY, nome TEXT NOT NULL, email TEXT NOT NULL UNIQUE, senha TEXT NOT NULL
+);
+
+CREATE TABLE categorias (
+    id SERIAL NOT NULL PRIMARY KEY, descricao TEXT NOT NULL
+);
+
+INSERT INTO
+    categorias (descricao)
+VALUES ('Informática'),
+    ('Celulares'),
+    ('Beleza e Perfumaria'),
+    ('Mercado'),
+    ('Livros e Papelaria'),
+    ('Brinquedos'),
+    ('Moda'),
+    ('Bebê'),
+    ('Games');
+
+CREATE TABLE produtos (
+    id SERIAL PRIMARY KEY NOT NULL, descricao TEXT NOT NULL, quantidade_estoque INTEGER NOT NULL, valor INTEGER NOT NULL, categoria_id INTEGER REFERENCES categorias (id)
+);
+
+CREATE TABLE clientes (
+    id SERIAL PRIMARY KEY NOT NULL, nome TEXT NOT NULL, email TEXT NOT NULL UNIQUE, cpf TEXT NOT NULL UNIQUE, cep TEXT NOT NULL, rua TEXT NOT NULL, numero TEXT NOT NULL, bairro TEXT NOT NULL, cidade TEXT NOT NULL, estado TEXT NOT NULL
+);
