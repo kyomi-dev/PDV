a4b5f41c1180cc344afad9480dfff0279670a379
kyomi <104471832+kyomi-dev@users.noreply.github.com>
2024-01-30 17:57:52 -0300

    fix: Corrige campos duplicados

--- tabelas.sql
+++ tabelas.sql
@@ -27,3 +27,29 @@ CREATE TABLE produtos (
 CREATE TABLE clientes (
     id SERIAL PRIMARY KEY NOT NULL, nome TEXT NOT NULL, email TEXT NOT NULL UNIQUE, cpf TEXT NOT NULL UNIQUE, cep TEXT, rua TEXT, numero TEXT, bairro TEXT, cidade TEXT, estado TEXT
 );
+
+CREATE TABLE pedidos (
+    id SERIAL PRIMARY KEY NOT NULL, cliente_id INTEGER REFERENCES clientes (id), observacao TEXT NOT NULL, valor_total DECIMAL NOT NULL
+);
+
+CREATE TABLE pedidos_produtos (
+    id SERIAL PRIMARY KEY NOT NULL, pedido_id INTEGER REFERENCES pedidos (id), produto_id INTEGER REFERENCES produtos (id), quantidade_produto INTEGER NOT NULL, valor_produto DECIMAL NOT NULL
+);
+
+ALTER TABLE produtos ADD produto_imagem TEXT;
+
+CREATE TABLE pedidos (
+    id SERIAL PRIMARY KEY NOT NULL, cliente_id INTEGER REFERENCES clientes (id), observacao TEXT NOT NULL, valor_total DECIMAL NOT NULL
+);
+
+CREATE TABLE pedidos_produtos (
+    id SERIAL PRIMARY KEY NOT NULL, pedido_id INTEGER REFERENCES pedidos (id), produto_id INTEGER REFERENCES produtos (id), quantidade_produto INTEGER NOT NULL, valor_produto DECIMAL NOT NULL
+);
+
+ALTER TABLE produtos ADD produto_imagem TEXT;
+
+ALTER TABLE pedidos_produtos RENAME TO pedido_produtos;
+
+ALTER TABLE pedido_produtos ALTER COLUMN valor_produto TYPE INTEGER;
+
+ALTER TABLE pedidos ALTER COLUMN valor_total TYPE INTEGER;
